73b173982f0f4a89bed207c18a70feefe057f9e0
tjstjdus <tjstjdus@gmail.com>
2026-06-09 11:45:10 +0900

    main.py

--- main.py
+++ main.py
@@ -90,8 +90,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://tjstjdus.github.io",
+        "http://localhost:3000",
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
